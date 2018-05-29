cf77a94430abf14ae2d875b23a915e09dc752c19
18goldr <18goldr@my.weston.org>
2018-05-29 14:38:37 -0400

    Moved everything up a folder level

---
+++ cmake-build-debug/CMakeFiles/pony_gp.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/main.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/main.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/binary_tree.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/binary_tree.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/config_parser.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/config_parser.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/csv_parser.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/csv_parser.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/file_util.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/file_util.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/hashmap.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/hashmap.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/memmngr.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/memmngr.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/misc_util.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/misc_util.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/queue.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/queue.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/rand_util.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/rand_util.c.o"
+  "/home/robbie/Dropbox/MIT/repos/pony_gp_c/util/tests.c" "/home/robbie/Dropbox/MIT/repos/pony_gp_c/cmake-build-debug/CMakeFiles/pony_gp.dir/util/tests.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ include/misc_util.h
@@ -0,0 +1,36 @@
+
+#ifndef PONY_GP_MISC_UTIL_H
+#define PONY_GP_MISC_UTIL_H
+
+#include <stdbool.h>
+#include <time.h>
+#include "../include/hashmap.h"
+/**
+ * A wrapper for the functions and terminals that the program can use.
+ * Keeps track of arities and the amount of functions and terminals available
+ * to the program.
+ * @field terminals An array of constants (single digit integers represented as characters).
+ * @field functions An array of functions (single characters).
+ * @field term_size The amount of constants available.
+ * @field func_size The amount of functions available.
+ * @field arities A hashmap containing the arities of the different functions.
+ */
+struct symbols {
+    char *terminals;
+    char *functions;
+    int term_size;
+    int func_size;
+    struct hashmap *arities;
+};
+
+bool symbol_is_valid(char sym, struct hashmap *arities);
+void remove_spaces(char *str);
+void remove_last_newline(char *str);
+void swap(int *a, int *b);
+double sum_doubles(double *values, int size);
+double get_std(double *values, int size, double ave);
+double *get_ave_and_std(double *values, int size);
+double max_value(double *values, int size);
+double get_time(void);
+
+#endif //PONY_GP_MISC_UTIL_H
